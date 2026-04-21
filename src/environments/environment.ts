50c2950c8fa20754c433d750d8836a3c7a11620d
Palani Kumar <palanikumar1817@gmail.com>
2026-04-21 23:21:12 +0530

    Firebase_Configuration

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    apiKey: "AIzaSyBSQmv3yacDa7g-FCdK2vvBcf48dvKLWF8",
+  authDomain: "trade-journal-6e777.firebaseapp.com",
+  projectId: "trade-journal-6e777",
+  storageBucket: "trade-journal-6e777.firebasestorage.app",
+  messagingSenderId: "348514541952",
+  appId: "1:348514541952:web:68ddd0e75df0f5fd0ca165",
+  measurementId: "G-9H9W8BM90J"
+  }
+};
